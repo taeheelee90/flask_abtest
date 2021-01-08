9cf682ae42a0fd54d647ed78780ebf5fc01ba481
taeheelee90 <taehee.lee90@gmail.com>
2021-01-08 15:44:14 +0200

    Store log in MongoDB

--- blog_view/blog.py
+++ blog_view/blog.py
@@ -1,4 +1,4 @@
-from flask import Flask, Blueprint, request, render_template, make_response, jsonify, redirect, url_for
+from flask import Flask, Blueprint, request, render_template, make_response, jsonify, redirect, url_for, session
 from flask_login import login_user, current_user, logout_user
 from blog_control.user_mgmt import User
 from blog_control.session_mgmt import BlogSession
@@ -9,11 +9,16 @@ blog_abtest = Blueprint('blog', __name__)
 
 @blog_abtest.route('/page')
 def page():
-    
     if current_user.is_authenticated:
-        return render_template('blog_A.html', user_email=current_user.user_email)
+        webpage_name = BlogSession.get_blog_page(current_user.blog_id)
+        BlogSession.save_session_info(
+            session['client_id'], current_user.user_email, webpage_name)
+        return render_template(webpage_name, user_email=current_user.user_email)
     else:
-        return render_template(BlogSession.get_blog_page())
+        webpage_name = BlogSession.get_blog_page()
+        BlogSession.save_session_info(
+            session['client_id'], 'anonymous', webpage_name)
+        return render_template(webpage_name)
 
 
 @blog_abtest.route('/set_email', methods=['GET', 'POST'])
